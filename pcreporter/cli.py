88c3d572e37c3b1522938de4b595e535a262b839
ayham-1 <me@ayham.xyz>
2024-07-15 06:36:34 +0200

    add error handler

--- pcreporter/cli.py
+++ pcreporter/cli.py
@@ -1,3 +1,4 @@
+import traceback
 import logging
 import os
 
@@ -48,6 +49,27 @@ async def cmd_usb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_html(info_usb())
 
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    if context is None:
+        return
+
+    # Log the error before we do anything else, so we can see it even if something breaks.
+
+    logger.error("Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+
+    # list of strings rather than a single string, so we have to join them together.
+
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+
+    tb_string = "".join(tb_list)
+    logger.error(tb_string)
+
+
 def main():
     """Start the bot."""
     # Create the Application and pass it your bot's token.
@@ -59,6 +81,7 @@ def main():
         return
 
     application = Application.builder().token(token).build()
+    application.add_error_handler(error_handler)
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
--- taskell.md
+++ taskell.md
@@ -1,15 +1,16 @@
 ## To Do
 
 - gps-monitor
-- gps-reporter
 - process-table watcher
 - mode switching
     > message sent to telegram to employ defensive tactics
 - integration with sway stuff
 - chmod executable bit switch monitor
+- features togglable with buttons
 
 ## Doing
 
+- gps-reporter
 
 ## Done
 
